7eef3e2744e576febfc75170964b9457fa52b694
Pierre <126479653+PierreMaze@users.noreply.github.com>
2025-11-27 17:01:56 +0100

    Update scraper.config.js

--- app/src/features/scraper/scraper.config.js
+++ app/src/features/scraper/scraper.config.js
@@ -3,6 +3,4 @@
 export const ScraperConfig = {
   delayBetweenRequests: 1500,
   retryAttempts: 2,
-  logSuccessMessage: (ref) => `Le produit n°${ref} a été trouvé et archivé ✅`,
-  logFailMessage: (ref) => `Le produit n°${ref} n'existe pas ❌`,
 };
